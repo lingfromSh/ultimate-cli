3752fb4c2656fbd6cc74e51dd05935d2b992675f
Stephen Ling <lingfromsh@163.com>
2020-01-31 16:57:49 +0800

    Add valid component for cli to call.

---
+++ components/__init__.py
@@ -0,0 +1,14 @@
+'''
+Filename: /home/ling/文档/ultimate-cli/components/__init__.py
+Path: /home/ling/文档/ultimate-cli/components
+Created Date: Friday, January 31st 2020, 10:53:53 am
+Author: Stephen Ling
+
+Copyright (c) 2020 Stephen Ling
+'''
+
+from .text import TextComponent
+
+__all__ = [
+    TextComponent
+]
